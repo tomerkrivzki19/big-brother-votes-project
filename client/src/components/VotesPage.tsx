0fcd25b584f858d9ef230d224147a5e3fb9d3908
tomerkrivzki19 <133903404+tomerkrivzki19@users.noreply.github.com>
2023-09-16 12:53:11 +0300

    build the system of counting votes, and fixed bugs

--- client/src/components/VotesPage.tsx
+++ client/src/components/VotesPage.tsx
@@ -1,15 +1,192 @@
-import React from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import NavVotes from "./nav-main/NavVotes";
+import modalStyle from "./modal/modalStyle";
+import Modal from "react-modal";
 
 function VotesPage() {
+  const [numberOfVotes, setNumberOfVotes] = useState(10);
+  const [countPlus, setCountPlus] = useState(0);
+
+  const [showModal, setShowModal] = useState(false);
+  const handleClick = useCallback(() => {
+    setShowModal(!showModal);
+  }, [showModal]);
+  const style: any = modalStyle;
+
+  const [disabled, setDisabled] = useState(false);
+
+  //   contenders
+  const [voteOne, setVoteOne] = useState(0);
+  const [voteTwo, setVoteTwo] = useState(0);
+  const [voteThree, setVoteThree] = useState(0);
+  const [voteFour, setVoteFour] = useState(0);
+  const [voteFive, setVoteFive] = useState(0);
+
+  useEffect(() => {
+    if (numberOfVotes < 1) {
+      //   alert(" הצבעה");
+      setNumberOfVotes(0);
+
+      setDisabled(true);
+      //   setVoteOne(voteOne);
+      //   setVoteTwo(voteTwo);
+      //   setVoteThree(voteThree);
+      //   setVoteFour(voteFour);
+      //   setVoteFive(voteFive);
+    } else {
+      setDisabled(false);
+    }
+  }, [numberOfVotes]);
+
+  useEffect(() => {
+    setShowModal(true);
+  }, []);
+
+  //   ---------1
+  const handleCountPlusVoteOne = () => {
+    if (voteOne <= 9) {
+      setVoteOne(voteOne + 1);
+      setNumberOfVotes(numberOfVotes - 1);
+    } else if (voteOne > 9) {
+      setVoteOne(10);
+
+      alert("עברת את כמות ההצבעות");
+    }
+  };
+
+  const handleCountMinusVoteOne = () => {
+    if (voteOne < 1) {
+      setVoteOne(0);
+    } else if (voteOne <= 10) {
+      setVoteOne(voteOne - 1);
+      setNumberOfVotes(numberOfVotes + 1);
+    } else if (numberOfVotes == 0) {
+      setNumberOfVotes(0);
+    }
+  };
+  //   ---------2
+  const handleCountPlusVoteTwo = () => {
+    if (voteTwo <= 9) {
+      setVoteTwo(voteTwo + 1);
+      setNumberOfVotes(numberOfVotes - 1);
+    } else if (voteTwo > 9) {
+      setVoteTwo(10);
+      setNumberOfVotes(0);
+
+      alert("עברת את כמות ההצבעות");
+    }
+  };
+
+  const handleCountMinusVoteTwo = () => {
+    if (voteTwo < 1) {
+      setVoteTwo(0);
+    } else if (voteTwo <= 10) {
+      setVoteTwo(voteTwo - 1);
+      setNumberOfVotes(numberOfVotes + 1);
+    } else if (numberOfVotes == 0) {
+      setNumberOfVotes(0);
+    }
+  };
+  //   ---------3
+  const handleCountPlusVoteThree = () => {
+    if (voteThree <= 9) {
+      setVoteThree(voteThree + 1);
+      setNumberOfVotes(numberOfVotes - 1);
+    } else if (voteThree > 9) {
+      setVoteThree(10);
+      setNumberOfVotes(0);
+
+      alert("עברת את כמות ההצבעות");
+    }
+  };
+
+  const handleCountMinusVoteThree = () => {
+    if (voteThree < 1) {
+      setVoteThree(0);
+    } else if (voteThree <= 10) {
+      setVoteThree(voteThree - 1);
+      setNumberOfVotes(numberOfVotes + 1);
+    } else if (numberOfVotes == 0) {
+      setNumberOfVotes(0);
+    }
+  };
+  //   ---------4
+  const handleCountPlusVoteFour = () => {
+    if (voteFour <= 9) {
+      setVoteFour(voteFour + 1);
+      setNumberOfVotes(numberOfVotes - 1);
+    } else if (voteFour > 9) {
+      setVoteFour(10);
+      setNumberOfVotes(0);
+
+      alert("עברת את כמות ההצבעות");
+    }
+  };
+
+  const handleCountMinusVoteFour = () => {
+    if (voteFour < 1) {
+      setVoteFour(0);
+    } else if (voteFour <= 10) {
+      setVoteFour(voteFour - 1);
+      setNumberOfVotes(numberOfVotes + 1);
+    } else if (numberOfVotes == 0) {
+      setNumberOfVotes(0);
+    }
+  };
+  //   ---------5
+  const handleCountPlusVoteFive = () => {
+    if (voteFive <= 9) {
+      setVoteFive(voteFive + 1);
+      setNumberOfVotes(numberOfVotes - 1);
+    } else if (voteFive > 9) {
+      setVoteFive(10);
+      setNumberOfVotes(0);
+
+      alert("עברת את כמות ההצבעות");
+    }
+  };
+
+  const handleCountMinusVoteFive = () => {
+    if (voteFive < 1) {
+      setVoteFive(0);
+    } else if (voteFive <= 10) {
+      setVoteFive(voteFive - 1);
+      setNumberOfVotes(numberOfVotes + 1);
+    } else if (numberOfVotes == 0) {
+      setNumberOfVotes(0);
+    }
+  };
+
   return (
     <>
+      {showModal && (
+        <Modal
+          isOpen={showModal}
+          onRequestClose={handleClick}
+          style={style}
+          ariaHideApp={false}
+        >
+          <form action="">
+            <h3>הירשמו והצביעו</h3>
+            <label htmlFor="">שם פרטי</label>
+            <input type="text" />
+            <label htmlFor="">שם משפחה</label>
+            <input type="text" />
+
+            <label htmlFor="">מספר -פאלפון</label>
+            <input type="tel" size={20} maxLength={10} required />
+            <button>הירשמ/י</button>
+          </form>
+        </Modal>
+      )}
       <div className="main-page-wraper">
         <div className="main-page-container">
           <NavVotes />
           <div className="main-content-header">
             <h2>מי יהיה המנצח של העונה?</h2>
-            <h3>נותרו 0 הצבעות</h3>
+            <h3>
+              נותרו <b>{numberOfVotes}</b> הצבעות
+            </h3>
           </div>
           <div className="select-celebrity-options">
             <div className="option">
@@ -18,9 +195,11 @@ function VotesPage() {
                 alt=""
               />
               <div className="vote-container">
-                <button>-</button>
-                <input type="button" placeholder="מספר הצבעות" />
-                <button>+</button>
+                <button onClick={handleCountMinusVoteOne}>-</button>
+                <input type="" placeholder={`${voteOne}`} readOnly />
+                <button onClick={handleCountPlusVoteOne} disabled={disabled}>
+                  +
+                </button>
               </div>
             </div>
             <div className="option">
@@ -29,9 +208,11 @@ function VotesPage() {
                 alt=""
               />
               <div className="vote-container">
-                <button>-</button>
-                <input type="button" placeholder="מספר הצבעות" />
-                <button>+</button>
+                <button onClick={handleCountMinusVoteTwo}>-</button>
+                <input type="" placeholder={`${voteTwo}`} readOnly />
+                <button onClick={handleCountPlusVoteTwo} disabled={disabled}>
+                  +
+                </button>
               </div>
             </div>
             <div className="option">
@@ -40,9 +221,11 @@ function VotesPage() {
                 alt=""
               />
               <div className="vote-container">
-                <button>-</button>
-                <input type="button" placeholder="מספר הצבעות" />
-                <button>+</button>
+                <button onClick={handleCountMinusVoteThree}>-</button>
+                <input type="" placeholder={`${voteThree}`} readOnly />
+                <button onClick={handleCountPlusVoteThree} disabled={disabled}>
+                  +
+                </button>
               </div>
             </div>
             <div className="option">
@@ -51,9 +234,11 @@ function VotesPage() {
                 alt=""
               />
               <div className="vote-container">
-                <button>-</button>
-                <input type="button" placeholder="מספר הצבעות" />
-                <button>+</button>
+                <button onClick={handleCountMinusVoteFour}>-</button>
+                <input type="" placeholder={`${voteFour}`} readOnly />
+                <button onClick={handleCountPlusVoteFour} disabled={disabled}>
+                  +
+                </button>
               </div>
             </div>
             <div className="option">
@@ -62,9 +247,11 @@ function VotesPage() {
                 alt=""
               />
               <div className="vote-container">
-                <button>-</button>
-                <input type="button" placeholder="מספר הצבעות" />
-                <button>+</button>
+                <button onClick={handleCountMinusVoteFive}>-</button>
+                <input type="" placeholder={`${voteFive}`} readOnly />
+                <button onClick={handleCountPlusVoteFive} disabled={disabled}>
+                  +
+                </button>
               </div>
             </div>
           </div>
---
+++ client/src/components/modal/modalStyle.ts
@@ -0,0 +1,20 @@
+const style = {
+  overlay: {
+    backdropFilter: "blur(3px)",
+  },
+  content: {
+    top: "45%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    maxWidth: "100%",
+    width: "20em",
+    backgroundColor: "white",
+    display: "flex",
+    justifyContent: "center",
+    paddingTop: "1em",
+    paddingRight: "1em",
+    direction: "rtl",
+    overflowY: "hidden",
+  },
+};
+export default style;
